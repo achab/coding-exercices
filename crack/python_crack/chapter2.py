6a2c8b00572c70ead5e6bca994ecc693bdf10b6a
Massil <massil.achab@gmail.com>
2015-11-17 01:15:13 +0100

    ...

--- crack/python_crack/chapter2.py
+++ crack/python_crack/chapter2.py
@@ -1,93 +1,49 @@
+from random import randint
 
-# coding: utf-8
-
-# In[31]:
-
-class Node(object):
-    
-    def __init__(self, data=None, next_node=None):
-        self.data = data
-        self.next_node = next_node
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
         
-    def get_data(self):
-        return self.data
-    
-    def get_next(self):
-        return self.next_node
-    
-    def set_next(self, new_next):
-        self.next_node = new_next
+    def __str__(self):
+        return str(self.value)
         
-class LinkedList(object):
-    
-    def __init__(self, head=None):
-        self.head = head
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
         
-    def insert(self, data):
-        new_node = Node(data)
-        new_node.set_next(self.head)
-        self.head = new_node
-        
-    def size(self):
-        current = self.head
-        count = 0
-        while current:
-            count += 1
-            current = current.get_next()
-        return count
-    
-    def search(self, data):
-        current = self.head
-        found = False
-        while current and not found:
-            if current.get_data() == data:
-                found = True
-            else:
-                current = current.next()
-        if current is None:
-            raise ValueError("Data is not in list.")
-        return current
-    
-    def contains(self, data):
-        current = self.head
-        found = False
-        while current and not found:
-            if current.get_data() == data:
-                found = True
-            else:
-                current = current.next()
-        return found
-    
-    def delete(self, data):
-        current = self.head
-        previous = None
-        found = False
-        while current and found is False:
-            if current.get_data() == data:
-                found = True
-            else:
-                previous = current
-                current = current.get_next()
-        if current is None:
-            raise ValueError("Data not in list")
-        if previous is None:
-            self.head = current.get_next()
+    def add_node(self, value):
+        node = Node(value)
+        if self.head == None:
+            self.head = node
+            self.tail = node
         else:
-            previous.set_next(current.get_next())
-
-
-# # exercise 2.1
-
-# In[ ]:
-
-#def remove_dups(L):
-#    d = {}
-#    while L:
-#        if L.data in d.keys():
-#            previous.next_node = L.next_node
-
-
-# In[ ]:
-
-
-
+            self.tail.next = node
+            self.tail = node
+            
+    def __str__(self):
+        if self.head != None:
+            index = self.head
+            nodestore = [str(index.value)]
+            while index.next != None:
+                index = index.next
+                nodestore.append(str(index.value))
+            return "LinkedList  [ " + "->".join(nodestore) + " ]"
+        return "LinkedList []"
+    
+    def remove_node(self, node_value):
+        current = self.head
+        if current.value == node_value:
+            self.head = self.head.next
+        while current.next != None:
+            if current.next.value == node_value:
+                current.next = current.next.next
+            else:
+                current = current.next
+                
+def random_LinkedList(length, min, max):
+    linkedlist = LinkedList()
+    for i in range(length):
+        linkedlist.add_node(randint(min,max))
+    return linkedlist
